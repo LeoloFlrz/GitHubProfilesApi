ebeca14f32496bf7b79967c75d41f467b290afbc
LeoloFlrz <leoloflrzr@gmail.com>
2023-06-08 20:22:48 +0200

    08/06 added async function

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
 const APIURL = 'https://api.github.com/users/'
 const main = document.getElementById("main");
+
+generateProfile();
+
+async function generateProfile() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        },
+    }
+
+    const res = await fetch(APIURL, config)
+
+    const data = await res.json()
+
+    main.innerHTML = data.profile;
+}
+
+main.addEventListener("keydown", (evento) =>{
+    if (evento.keycode === 13) {
+        let inputData = document.getElementById("search").value;
+        
+    }
+})
