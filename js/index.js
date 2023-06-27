aae319f7cfd95197e7de29c2ef6c35f8d0345792
LeoloFlrz <leoloflrzr@gmail.com>
2023-06-27 18:33:41 +0200

    added try catch

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = "https://api.github.com/users/";
 const main = document.getElementById("main");
 
-generateProfile();
+main.addEventListener("keydown", (evento) => {
+  if (evento.keycode === 13) {
+    let inputData = document.getElementById("search").value;
+    return inputData;
+  }
+  return inputData;
+});
 
-async function generateProfile() {
-    const config = {
-        headers: {
-            Accept: 'application/json',
-        },
-    }
+async function generateProfile(inputData) {
+  try {
+    const res = await fetch(APIURL+inputData);
+    console.log(res);
 
-    const res = await fetch(APIURL, config)
+    const data = await res.json();
 
-    const data = await res.json()
-
-    main.innerHTML = data.profile;
+    main.innerHTML = data;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-main.addEventListener("keydown", (evento) =>{
-    if (evento.keycode === 13) {
-        let inputData = document.getElementById("search").value;
-        
-    }
-})
+generateProfile();
