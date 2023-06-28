0cf4192e14979b1181c7c5716b8e22b548ad6a75
Leolo Flórez <116220158+LeoloFlrz@users.noreply.github.com>
2023-06-28 08:54:23 +0200

    28/06

--- js/index.js
+++ js/index.js
@@ -1,25 +1,26 @@
-const APIURL = 'https://api.github.com/users/'
+const APIURL = `https://api.github.com/users/${inputData}`;
 const main = document.getElementById("main");
 
 generateProfile();
 
 async function generateProfile() {
-    const config = {
-        headers: {
-            Accept: 'application/json',
-        },
-    }
+  const config = {
+    headers: {
+      Accept: "application/json",
+    },
+  };
 
-    const res = await fetch(APIURL, config)
+  const res = await fetch(APIURL, config)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+  const data = await res.json();
 
-    const data = await res.json()
-
-    main.innerHTML = data.profile;
+  main.innerHTML = data.profile;
 }
 
-main.addEventListener("keydown", (evento) =>{
-    if (evento.keycode === 13) {
-        let inputData = document.getElementById("search").value;
-        
-    }
-})
+main.addEventListener("keydown", (evento) => {
+  if (evento.keycode === 13) {
+    let inputData = document.getElementById("search").value;
+    return inputData;
+  }
+});
